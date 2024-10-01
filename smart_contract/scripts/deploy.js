13b7419b564af91dc62f67430680e5f53302c188
Abhishek Tripathi <abhishektripathi@Abhisheks-MacBook-Air.local>
2024-10-01 16:57:22 +0530

    update

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -3,10 +3,7 @@ const { ethers } = require("hardhat");
 async function main() {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", deployer.address);
-
   const Transactions = await ethers.getContractFactory("Transactions");
-
-  // Set maxFeePerGas and maxPriorityFeePerGas for EIP-1559
   const maxPriorityFeePerGas = ethers.utils.parseUnits('20', 'gwei'); // 20 gwei priority fee
   const maxFeePerGas = ethers.utils.parseUnits('40', 'gwei'); // 40 gwei max fee (base fee + priority fee)
   
@@ -18,7 +15,6 @@ async function main() {
 
   console.log("Waiting for deployment transaction to be mined...");
   await transactions.deployed();
-
   console.log("Transactions contract deployed to:", await transactions.address);
 }
 
